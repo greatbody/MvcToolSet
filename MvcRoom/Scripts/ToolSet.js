b0c03bd4c51d2d0b13589c58b3068416549de6ae
greatbody <wedown@163.com>
2015-03-20 17:47:53 +0800

    新增自动的SQL处理。
    修复：导航滑动的效果

--- MvcRoom/Scripts/ToolSet.js
+++ MvcRoom/Scripts/ToolSet.js
@@ -1,4 +1,25 @@
-﻿var command = {
+﻿//***************公共函数*****************
+Public = {
+    moveToID: function (id, offset) {
+        ///<param name="id">需要移动到的对象的id</param>
+        ///<param name="offset">对象的漂移量</param>
+        var o = $("#" + id);
+        if (o.length == 0) {
+            return;
+        }
+        $("body").animate({ scrollTop: o.offset().top - 50 });
+    },
+    getFirstOneSubFit: function (regstr, text) {
+        var oReg = new RegExp(regstr, "g");
+        var arrCollection = oReg.exec(text);
+        if (arrCollection.length >= 2) {
+            return arrCollection[1];
+        } else {
+            return arrCollection[0];
+        }
+    }
+};
+var command = {
     action: {
         create: function (functioncode, actioncode, sql) {
             var sOriginCode = $("#templete_sql").html();
@@ -8,21 +29,27 @@
             code = code.replace(/{objecttype}/g, functioncode);
             code = code.replace(/{actioncode}/g, actioncode);
             $("#ActionSql_Result").val(code);
+        },
+        autofill: function (sql) {
+            var sFunctionCode = Public.getFirstOneSubFit("\\'([0-9]{8}?)\\'", sql);
+            var sActionCode = Public.getFirstOneSubFit("\\'([0-9]{2,3}?)\\'", sql);
+            var sActionSql = sql;
+            $("#FunctionCode").val(sFunctionCode);
+            $("#ActionCode").val(sActionCode);
+            this.create(sFunctionCode, sActionCode, sActionSql);
         }
     }
 };
+//第一个，创建功能点SQL的按钮，id=btnFunctionAdd
 $("#btnFunctionAdd").click(function () {
     command.action.create($("#FunctionCode").val(), $("#ActionCode").val(), $("#ActionSql").val());
 });
-
+$("#ActionSql").keyup(function () {
+    command.action.autofill($("#ActionSql").val());
+});
+//文本区点击后自动全选
 $("textarea[id='ActionSql_Result']").click(function () {
     $(this).select();
 });
 
-function moveToXY(id) {
-    var o = $("#" + id);
-    if (o.length == 0) {
-        return;
-    }
-    $("body").animate({ scrollTop: o.offset().top-50});
-}
+
